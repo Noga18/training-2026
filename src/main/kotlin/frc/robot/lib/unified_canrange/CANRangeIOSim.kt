1bdce0f9ffdedc84be54cd5dfd24794800841e13
rakrakon <68773850+rakrakon@users.noreply.github.com>
2025-07-22 19:28:50 +0300

    Log tuning under subsystem

--- src/main/kotlin/frc/robot/lib/unified_canrange/CANRangeIOSim.kt
+++ src/main/kotlin/frc/robot/lib/unified_canrange/CANRangeIOSim.kt
@@ -2,9 +2,9 @@ package frc.robot.lib.unified_canrange
 
 import org.littletonrobotics.junction.networktables.LoggedNetworkBoolean
 
-class CANRangeIOSim: CANRangeIO {
+class CANRangeIOSim(subsystemName: String): CANRangeIO {
     override val inputs = CANRangeIO.SensorInputs()
-    private val isDetecting = LoggedNetworkBoolean("/Tuning/IsDetecting", false)
+    private val isDetecting = LoggedNetworkBoolean("/Tuning/$subsystemName/IsDetecting", false)
 
     override fun updateInputs() {
         inputs.isDetecting = isDetecting.get()
