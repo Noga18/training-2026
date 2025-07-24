510f05cfa65e3985722986308c060cd7a6b566ae
Noga18 <noga.veksler@gmail.com>
2025-07-24 18:42:51 +0300

    Update src/main/kotlin/frc/robot/subsystems/wrist/WristConstants.kt
    Co-authored-by: Dan Katzuv <31829093+katzuv@users.noreply.github.com>

--- src/main/kotlin/frc/robot/subsystems/wrist/WristConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/wrist/WristConstants.kt
@@ -4,7 +4,7 @@ import edu.wpi.first.units.measure.Angle
 import frc.robot.lib.extensions.degrees
 
 enum class Angles(degrees: Angle) {
-    resetAngle(0.0.degrees),
-    openAngle(0.0.degrees),
-    closeAngle(0.0.degrees)
+    RESET(0.deg),
+    CLOSE(0.deg),
+    OPEN(0.deg)
 }
