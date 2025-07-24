d94673695820c4da165b3c6bc0ef72f94e8747dc
Noga18 <noga.veksler@gmail.com>
2025-07-24 18:44:34 +0300

    Update src/main/kotlin/frc/robot/subsystems/wrist/Wrist.kt
    Co-authored-by: Barak Bornstein <68773850+rakrakon@users.noreply.github.com>

--- src/main/kotlin/frc/robot/subsystems/wrist/Wrist.kt
+++ src/main/kotlin/frc/robot/subsystems/wrist/Wrist.kt
@@ -48,6 +48,6 @@ class Wrist : SubsystemBase() {
 
     override fun periodic() {
         motor.updateInputs()
-        Logger.processInputs("subsystem/$name", motor.inputs)
+        Logger.processInputs("Subsystems/$name", motor.inputs)
     }
 }
