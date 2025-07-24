aa09d4b256cbd068aa5ce471fb47d3fe3dac41ba
יהלי <mor.yahali1612@gmail.com>
2025-07-24 18:53:35 +0300

    SysIdable

--- src/main/kotlin/frc/robot/subsystems/shooter/flywheel/Flywheel.kt
+++ src/main/kotlin/frc/robot/subsystems/shooter/flywheel/Flywheel.kt
@@ -1,6 +1,7 @@
 package frc.robot.subsystems.shooter.flywheel
 
 import com.ctre.phoenix6.controls.VelocityTorqueCurrentFOC
+import com.ctre.phoenix6.controls.VoltageOut
 import edu.wpi.first.units.measure.AngularVelocity
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.SubsystemBase
@@ -8,8 +9,10 @@ import edu.wpi.first.wpilibj2.command.button.Trigger
 import frc.robot.lib.universal_motor.UniversalTalonFX
 import org.littletonrobotics.junction.Logger
 
-class Flywheel : SubsystemBase() {
+class Flywheel : SubsystemBase(), SysIdable {
     private val velocityTorque = VelocityTorqueCurrentFOC(0.0)
+    private val velocityVoltage = VoltageOut(0.0)
+
     private var velocitySetpoint = STOP_VELOCITY
     private val motor = UniversalTalonFX(port, config = MOTOR_CONFIG)
 
@@ -28,5 +31,9 @@ class Flywheel : SubsystemBase() {
         Logger.processInputs("Subsystem/$name", motor.inputs)
         Logger.recordOutput("FlyWheel/IsAtSetVelocity", isAtSetVelocity)
         Logger.recordOutput("FlyWheel/SetVelocity", velocitySetpoint)
+    }
+
+    override fun setVoltage(voltage: Voltage) {
+        mainMotor.setControl(VoltageOut(voltage))
     }
 }
