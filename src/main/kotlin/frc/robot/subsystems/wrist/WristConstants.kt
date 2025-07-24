3a86a5b07c8861e797d2893a36de5ca29d6f8499
nogav <noga.veksler@gmail.com>
2025-07-24 17:46:34 +0300

    Add WristConstants.kt

---
+++ src/main/kotlin/frc/robot/subsystems/wrist/WristConstants.kt
@@ -0,0 +1,12 @@
+package frc.robot.subsystems.wrist
+
+import edu.wpi.first.units.measure.Angle
+import frc.robot.lib.extensions.degrees
+
+enum class Angles(degrees: Angle) {
+     resetAngle(0.0.degrees),
+     openAngle(0.0.degrees),
+    closeAngle(0.0.degrees)
+
+
+}
