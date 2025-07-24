74c2a50dca068c250405b3924ab6cdbd10e42cd3
nogav <noga.veksler@gmail.com>
2025-07-24 18:57:00 +0300

    Rename things

--- src/main/kotlin/frc/robot/subsystems/wrist/WristConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/wrist/WristConstants.kt
@@ -1,10 +1,12 @@
 package frc.robot.subsystems.wrist
 
 import edu.wpi.first.units.measure.Angle
+import frc.robot.lib.extensions.deg
 import frc.robot.lib.extensions.degrees
 
+ const val GEAR_RATIO = 1/69.82
+
 enum class Angles(degrees: Angle) {
-    RESET(0.deg),
     CLOSE(0.deg),
     OPEN(0.deg)
 }
