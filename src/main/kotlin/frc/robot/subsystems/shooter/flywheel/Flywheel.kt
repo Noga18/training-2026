bc9be7f34ceddf305737839071ea7eace5827bac
יהלי <mor.yahali1612@gmail.com>
2025-07-24 18:40:38 +0300

    SysIdable

--- src/main/kotlin/frc/robot/subsystems/shooter/flywheel/Flywheel.kt
+++ src/main/kotlin/frc/robot/subsystems/shooter/flywheel/Flywheel.kt
@@ -1,17 +1,27 @@
 package frc.robot.subsystems.shooter.flywheel
 
 import com.ctre.phoenix6.controls.VelocityTorqueCurrentFOC
+import com.ctre.phoenix6.controls.VoltageOut
 import edu.wpi.first.units.measure.AngularVelocity
+import edu.wpi.first.units.measure.Voltage
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import edu.wpi.first.wpilibj2.command.button.Trigger
+import frc.robot.lib.sysid.SysIdable
 import frc.robot.lib.universal_motor.UniversalTalonFX
 import org.littletonrobotics.junction.Logger
 
-class Flywheel : SubsystemBase() {
+class Flywheel : SubsystemBase(), SysIdable {
     private val velocityTorque = VelocityTorqueCurrentFOC(0.0)
     private var velocitySetpoint = STOP_VELOCITY
     private val motor = UniversalTalonFX(port, config = MOTOR_CONFIG)
+    private val velocityVoltage = VoltageOut(0.0)
+
+    private var velocitySetpoint = 0.rps
+    private val mainMotor =
+        UniversalTalonFX(MAIN_MOTOR_PORT, config = MOTOR_CONFIG)
+    private val auxMotor =
+        UniversalTalonFX(AUX_MOTOR_PORT, config = MOTOR_CONFIG)
 
     val isAtSetVelocity = Trigger { motor.inputs.velocity == velocitySetpoint }
 
@@ -28,5 +38,9 @@ class Flywheel : SubsystemBase() {
         Logger.processInputs("Subsystem/$name", motor.inputs)
         Logger.recordOutput("FlyWheel/IsAtSetVelocity", isAtSetVelocity)
         Logger.recordOutput("FlyWheel/SetVelocity", velocitySetpoint)
+    }
+
+    override fun setVoltage(voltage: Voltage) {
+        mainMotor.setControl(VoltageOut(voltage))
     }
 }
